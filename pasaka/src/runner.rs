21dbdd2df1ccb55a540d916f7258f77f22ab0363
Julius Gvozdiovas <git@julgvoz.com>
2025-08-30 15:18:10 +0200

    refactor: Extend Runner trait with save/load methods

--- pasaka/src/runner.rs
+++ pasaka/src/runner.rs
@@ -1,7 +1,13 @@
+use serde::Serialize;
+
 use crate::choice::{Choice, ChoiceResult};
 
 pub mod cli;
 
 pub trait Runner {
     fn render_choice(&mut self, choice: Choice) -> impl IntoFuture<Output = Option<ChoiceResult>>;
+
+    fn save<T: Serialize>(&mut self, key: &str, value: T) -> impl Future<Output = bool>;
+
+    fn load<T: Serialize>(&mut self, key: &str) -> impl Future<Output = Option<T>>;
 }
--- pasaka/src/runner/cli.rs
+++ pasaka/src/runner/cli.rs
@@ -1,5 +1,5 @@
 use console::Term;
-use dialoguer::Select;
+use dialoguer::{Select, theme::ColorfulTheme};
 
 use crate::{
     choice::{Choice, ChoiceResult},
@@ -8,8 +8,8 @@ use crate::{
 
 pub struct CliRunner;
 
+#[allow(refining_impl_trait)]
 impl Runner for CliRunner {
-    #[allow(refining_impl_trait)]
     async fn render_choice(&mut self, choice: Choice) -> Option<ChoiceResult> {
         Term::stdout().clear_screen().unwrap();
 
@@ -22,7 +22,7 @@ impl Runner for CliRunner {
             return None;
         }
 
-        let index: usize = Select::new()
+        let index: usize = Select::with_theme(&ColorfulTheme::default())
             .default(0)
             .items(choice.labels)
             .interact()
@@ -31,5 +31,13 @@ impl Runner for CliRunner {
         let result: ChoiceResult = (choice.action)(index);
 
         Some(result)
+    }
+
+    async fn save<T: serde::Serialize>(&mut self, key: &str, value: T) -> bool {
+        todo!()
+    }
+
+    async fn load<T: serde::Serialize>(&mut self, key: &str) -> Option<T> {
+        todo!()
     }
 }
